70a721000e72a8082da1302a2f49c593d5c47f2e
perryratcliff <perryratcliff@gmail.com>
2016-10-22 12:35:23 -0500

    update client with notes

--- src/schemas/client.js
+++ src/schemas/client.js
@@ -11,17 +11,17 @@ const clientSchema = new Schema({
     type: String,
     enum: ['Native American', 'Asian', 'Black', 'Caucasian', 'Hispanic', 'Other']
   },
+  disabilities: [
+    {
+      disabilityType: Number,
+      receivingServices: Number
+    }
+  ],
 
   /* House Info */
   home: { type: String, enum: ['House', 'Apartment'] },
   rent: Number,
   lease: String,
-
-  /* Meta */
-  status: { type: String, enum: ['pending', 'approved'] },
-  user: { type: Schema.Types.ObjectId, ref: 'User' },
-  invite: { type: Schema.Types.ObjectId, ref: 'Invite' },
-  paymentHistory: [{ type: Schema.Types.ObjectId, ref: 'Payment' }],
 
   /* Veteran Status */
   veteran: Boolean,
@@ -29,21 +29,22 @@ const clientSchema = new Schema({
     type: String,
     enum: ['World War II', 'Korean War', 'Vietnam War', 'Desert Storm', 'Afghan', 'Iraq', 'Other']
   }],
+
+  /* Fiscal Info */
+  employed: Boolean,
   earned_income: Number,
   benefits_income: Number,
 
-  disabilities: [
-    {
-      disabilityType: Number,
-      receivingServices: Number
-    }
-  ],
-  education: [
-    {
-      lastGradeCompleted: Number,
-      schoolStatus: String
-    }
-  ],
+  /* Education */
+  lastGradeCompleted: Number,
+  schoolStatus: String,
+
+
+  /* Meta */
+  status: { type: String, enum: ['pending', 'approved'] },
+  user: { type: Schema.Types.ObjectId, ref: 'User' },
+  invite: { type: Schema.Types.ObjectId, ref: 'Invite' },
+  paymentHistory: [{ type: Schema.Types.ObjectId, ref: 'Payment' }],
   notes: [{ type: Schema.Types.ObjectId, ref: 'Note' }]
 }, {
   timestamps: true
