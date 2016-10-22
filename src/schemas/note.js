cbe4ebb2ba3df66954db32778e66cf176dd9fee5
perryratcliff <perryratcliff@gmail.com>
2016-10-22 12:20:03 -0500

    update note model

--- src/schemas/note.js
+++ src/schemas/note.js
@@ -3,10 +3,9 @@ import Mongoose from 'mongoose';
 const { Schema } = Mongoose;
 
 const noteSchema = new Schema({
-    type: { type: String, enum: ['Approve', 'Call - Response', 'Call - No Response', 'Comment'] },
-    body: String
+  note: { type: String },
 }, {
-    timestamps: true
+  timestamps: true
 });
 
 const Note = Mongoose.model('Note', noteSchema);
