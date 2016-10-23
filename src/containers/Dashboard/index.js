3679751af83acd908f535b82b09e7882a7f4e704
perryratcliff <perryratcliff@gmail.com>
2016-10-23 04:23:25 -0500

    fix up client dashboard

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,52 +1,48 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import colors from '../../styles/colors';
 
-import ClientInfo from '../../components/ClientInfo';
+import colors from '../../styles/colors';
 import PaymentHistory from '../../components/PaymentHistory';
-import { fetchClients } from '../../actions/clients';
+import { fetchClients, addPayment } from '../../actions/clients';
 
 class Dashboard extends Component {
   componentDidMount() {
     this.props.dispatch(fetchClients());
   }
 
-  getStarObjects(star_count) {
-    let starsArray = [];
-    for (var i = 0; i < star_count; i++) {
-      starsArray.push({});
+  getStarObjects(completed) {
+    var stars = [];
+    for (let i = 0; i < 30; i++) {
+      stars.push(<i key={i} className="fa fa-star" style={{
+        fontSize: 13,
+        color: i < completed ? colors.primary : colors.light,
+        marginRight: 4,
+        marginBottom: 4
+      }}/>);
     }
-    console.log('fp', star_count, starsArray)
-    return starsArray;
+    return stars;
   }
 
   render() {
     const { client } = this.props;
-    const star_count = 13;
-    const levels = [];
-    for (var i = 0; i < Math.floor(star_count / 3); i++) {
-      levels.push({star_count: 3});
-    }
-    if (star_count % 3) {
-      levels.push({star_count: star_count % 3});
-    }
-
     return (
-      <div style={{margin: '30px 50px'}}>
-        <div style={{textAlign: 'center', fontSize: 30, lineHeight: '72px'}}>Welcome <span style={{color: colors.primary}}> {client.name}</span></div>
-        <div>
-          {levels.map((level, i) => (
-          <div key={i}>
-            <div>{20 - i}</div>
-            {this.getStarObjects(level.star_count).map(star => (
-            <div style={{display: 'inline'}}>*</div>
-            ))}
-          </div>
-          ))}
+      <div style={{borderTop: `1px solid ${colors.border}`}}>
+        <div style={{textAlign: 'center', fontSize: 30, lineHeight: '72px'}}>
+          Welcome <span style={{color: colors.primary}}>{client.name}</span>
         </div>
-        <PaymentHistory
-          monthly_home_payment={client.monthly_home_payment}
-          payment_history={client.payment_history}/>
+        <div style={{display: 'flex', flexWrap: 'wrap', width: 242, margin: 'auto'}}>
+          {this.getStarObjects(client.payment_stars)}
+        </div>
+        <div style={{display: 'flex', paddingTop: '32px', paddingRight: 40, paddingLeft: 40}}>
+          <div style={{flex: 1}}></div>
+          <div style={{flex: 1}}>
+            <PaymentHistory
+              handleAddPayment={amount => this.props.dispatch(addPayment(client._id, amount))}
+              monthlyHomePayment={client.monthly_home_payment || 0}
+              paymentHistory={client.payment_history || []}
+            />
+          </div>
+        </div>
       </div>
     );
   }
