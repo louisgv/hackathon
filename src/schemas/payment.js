5b6b3c5e5286b720cb96f6173c51bfe332e51ae6
perryratcliff <perryratcliff@gmail.com>
2016-10-22 21:43:31 -0500

    fix

--- src/schemas/payment.js
+++ src/schemas/payment.js
@@ -3,8 +3,8 @@ import Mongoose from 'mongoose';
 const { Schema } = Mongoose;
 
 const paymentSchema = new Schema({
-    amount: Number,
-    payed_on: {type: Date, default: Date.now()}
+  amount: Number,
+  payed_on: { type: Date, default: Date.now() }
 });
 
 export default paymentSchema;
